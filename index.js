16a3f2b3b4692b8991dbac26f76e1f81c38df13a
Vidar Eldøy <vidar@eldoy.com>
2026-01-20 22:39:51 +0100

    fix: tweak set

--- index.js
+++ index.js
@@ -1,21 +1,15 @@
 var nano = require('nano')
 
-var clean = require('./lib/clean.js')
-var uuid = require('./lib/uuid.js')
-var match = require('./lib/match.js')
-var ensure = require('./lib/ensure.js')
-
-var drop = require('./lib/api/drop.js')
-var index = require('./lib/api/index.js')
-var list = require('./lib/api/list.js')
-var batch = require('./lib/api/batch.js')
-var insert = require('./lib/api/insert.js')
-var update = require('./lib/api/update.js')
-var remove = require('./lib/api/remove.js')
-var count = require('./lib/api/count.js')
-
-var get = require('./lib/api/get.js')
-var set = require('./lib/api/set.js')
+var apiDrop = require('./lib/api/drop.js')
+var apiIndex = require('./lib/api/index.js')
+var apiList = require('./lib/api/list.js')
+var apiBatch = require('./lib/api/batch.js')
+var apiInsert = require('./lib/api/insert.js')
+var apiUpdate = require('./lib/api/update.js')
+var apiRemove = require('./lib/api/remove.js')
+var apiCount = require('./lib/api/count.js')
+var apiGet = require('./lib/api/get.js')
+var apiSet = require('./lib/api/set.js')
 
 async function webdb(url) {
   var client = nano(url)
@@ -28,16 +22,36 @@ async function webdb(url) {
     }
 
     return {
-      drop: drop.bind(null, ctx),
-      index: index.bind(null, ctx),
-      list: list.bind(null, ctx),
-      batch: batch.bind(null, ctx),
-      insert: insert.bind(null, ctx),
-      update: update.bind(null, ctx),
-      remove: remove.bind(null, ctx),
-      count: count.bind(null, ctx),
-      set: set.bind(null, ctx),
-      get: get.bind(null, ctx)
+      drop: function () {
+        return apiDrop(ctx)
+      },
+      index: function (spec) {
+        return apiIndex(ctx, spec)
+      },
+      list: function (query, options) {
+        return apiList(ctx, query, options)
+      },
+      batch: function (query, options, handler) {
+        return apiBatch(ctx, query, options, handler)
+      },
+      insert: function (values) {
+        return apiInsert(ctx, values)
+      },
+      update: function (query, values) {
+        return apiUpdate(ctx, query, values)
+      },
+      remove: function (query) {
+        return apiRemove(ctx, query)
+      },
+      count: function (query) {
+        return apiCount(ctx, query)
+      },
+      set: function (query, values) {
+        return apiSet(ctx, query, values)
+      },
+      get: function (query, options) {
+        return apiGet(ctx, query, options)
+      }
     }
   }
 
