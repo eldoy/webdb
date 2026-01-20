450a93c57d931054d61225cfd2af5b6cbc422def
Vidar Eldøy <vidar@eldoy.com>
2026-01-20 04:17:58 +0100

    fix: add index function

--- index.js
+++ index.js
@@ -1,171 +1,155 @@
 var nano = require('nano')
 
-module.exports = function (url) {
-  var server = nano(url)
+async function webdb(url) {
+  var client = nano(url)
 
-  function db(name) {
-    return collection(name, server)
+  var db = function (name) {
+    var handle
+    var ready
+
+    async function init() {
+      if (ready) return ready
+      ready = (async () => {
+        try {
+          await client.db.get(name)
+        } catch {
+          await client.db.create(name)
+        }
+        handle = client.db.use(name)
+        return handle
+      })()
+      return ready
+    }
+
+    function normalizeSort(sort) {
+      return Object.keys(sort).map((k) => ({
+        [k]: sort[k] === -1 ? 'desc' : 'asc'
+      }))
+    }
+
+    function normalizeFields(fields) {
+      var inc = []
+      for (var k in fields) {
+        if (fields[k]) inc.push(k === 'id' ? '_id' : k)
+      }
+      if (!inc.length) return undefined
+      if (!inc.includes('_id')) inc.push('_id')
+      return inc
+    }
+
+    function normalizeDocs(docs) {
+      for (var d of docs) {
+        d.id = d._id
+        delete d._id
+        delete d._rev
+      }
+    }
+
+    return {
+      async all(query = {}, opts = {}) {
+        var db = await init()
+        var q = { selector: query }
+
+        if (opts.sort) q.sort = normalizeSort(opts.sort)
+        if (opts.limit !== undefined) q.limit = opts.limit
+        if (opts.skip) q.skip = opts.skip
+        if (opts.fields) q.fields = normalizeFields(opts.fields)
+
+        var r = await db.find(q)
+        normalizeDocs(r.docs)
+        return r.docs
+      },
+
+      async get(query = {}) {
+        var r = await this.all(query, { limit: 1 })
+        return r[0] || null
+      },
+
+      async count(query = {}) {
+        var db = await init()
+        var r = await db.find({ selector: query })
+        return r.docs.length
+      },
+
+      async set(arg, values) {
+        var db = await init()
+
+        if (Array.isArray(arg)) {
+          var out = []
+          for (var d of arg) {
+            var r = await db.insert(d)
+            d.id = r.id
+            out.push(d)
+          }
+          return out
+        }
+
+        if (values === undefined) {
+          var r = await db.insert(arg)
+          arg.id = r.id
+          return arg
+        }
+
+        var found = await db.find({ selector: arg })
+
+        if (!found.docs.length) {
+          if (values === null) return { n: 0 }
+
+          var doc = {}
+          for (var k in arg) {
+            var v = arg[k]
+            if (v && typeof v === 'object' && '$eq' in v) doc[k] = v.$eq
+            else if (typeof v !== 'object') doc[k] = v
+          }
+          for (var k in values) {
+            if (values[k] !== undefined) doc[k] = values[k]
+          }
+
+          await db.insert(doc)
+          return { n: 1 }
+        }
+
+        if (values === null) {
+          await db.bulk({
+            docs: found.docs.map((d) => ({
+              _id: d._id,
+              _rev: d._rev,
+              _deleted: true
+            }))
+          })
+          return { n: found.docs.length }
+        }
+
+        var upd = []
+        for (var cur of found.docs) {
+          var next = { ...cur }
+          for (var k in values) {
+            if (values[k] === undefined) delete next[k]
+            else next[k] = values[k]
+          }
+          upd.push(next)
+        }
+
+        await db.bulk({ docs: upd })
+        return { n: upd.length }
+      },
+
+      async drop() {
+        try {
+          await client.db.destroy(name)
+        } catch {}
+        handle = null
+        ready = null
+      },
+
+      async index() {
+        return (await init()).createIndex.apply(handle, arguments)
+      }
+    }
   }
 
+  db.client = client
   return db
 }
 
-function collection(name, server) {
-  var handle
-  var initPromise
-
-  async function init() {
-    if (initPromise) return initPromise
-    initPromise = (async () => {
-      try {
-        await server.db.get(name)
-      } catch {
-        await server.db.create(name)
-      }
-      handle = server.db.use(name)
-      return handle
-    })()
-    return initPromise
-  }
-
-  function normalizeQuery(q) {
-    return q || {}
-  }
-
-  function normalizeSort(sort) {
-    if (!sort) return undefined
-    return Object.keys(sort).map((k) => ({
-      [k]: sort[k] === -1 ? 'desc' : 'asc'
-    }))
-  }
-
-  function normalizeFields(fields) {
-    if (!fields) return undefined
-    var inc = []
-    for (var k in fields) {
-      if (fields[k]) inc.push(k === 'id' ? '_id' : k)
-    }
-    if (!inc.length) return undefined
-    if (!inc.includes('_id')) inc.push('_id')
-    return inc
-  }
-
-  function normalizeDocs(docs) {
-    for (var d of docs) {
-      d.id = d._id
-      delete d._id
-      delete d._rev
-    }
-  }
-
-  async function all(query, opts = {}) {
-    var db = await init()
-    var q = { selector: normalizeQuery(query) }
-    if (opts.sort) q.sort = normalizeSort(opts.sort)
-    if (opts.limit !== undefined) q.limit = opts.limit
-    if (opts.skip) q.skip = opts.skip
-    if (opts.fields) q.fields = normalizeFields(opts.fields)
-
-    var r = await db.find(q)
-    normalizeDocs(r.docs)
-    return r.docs
-  }
-
-  async function get(query) {
-    var r = await all(query, { limit: 1 })
-    return r[0] || null
-  }
-
-  async function count(query) {
-    var db = await init()
-    var r = await db.find({ selector: normalizeQuery(query) })
-    return r.docs.length
-  }
-
-  async function set(arg, values) {
-    var db = await init()
-
-    if (Array.isArray(arg)) {
-      var out = []
-      for (var d of arg) {
-        var r = await db.insert(d)
-        d.id = r.id
-        out.push(d)
-      }
-      return out
-    }
-
-    if (values === undefined) {
-      var r = await db.insert(arg)
-      arg.id = r.id
-      return arg
-    }
-
-    var found = await db.find({ selector: normalizeQuery(arg) })
-
-    if (!found.docs.length) {
-      if (values === null) return { n: 0 }
-      var doc = {}
-      for (var k in arg) {
-        if (typeof arg[k] !== 'object' || arg[k].$eq !== undefined) {
-          doc[k] = arg[k].$eq ?? arg[k]
-        }
-      }
-      for (var k in values) {
-        if (values[k] !== undefined) doc[k] = values[k]
-      }
-      var ins = await db.insert(doc)
-      return { n: 1 }
-    }
-
-    if (values === null) {
-      var del = found.docs.map((d) => ({
-        _id: d._id,
-        _rev: d._rev,
-        _deleted: true
-      }))
-      await db.bulk({ docs: del })
-      return { n: del.length }
-    }
-
-    var upd = []
-    for (var cur of found.docs) {
-      var next = { ...cur }
-      for (var k in values) {
-        if (values[k] === undefined) delete next[k]
-        else next[k] = values[k]
-      }
-      upd.push(next)
-    }
-
-    await db.bulk({ docs: upd })
-    return { n: upd.length }
-  }
-
-  async function drop() {
-    try {
-      await server.db.destroy(name)
-    } catch {}
-    handle = null
-    initPromise = null
-  }
-
-  return {
-    all,
-    get,
-    count,
-    set,
-    drop,
-    data: new Proxy(
-      {},
-      {
-        get(_, prop) {
-          return async function () {
-            var db = await init()
-            return db[prop].apply(db, arguments)
-          }
-        }
-      }
-    )
-  }
-}
+module.exports = webdb
--- spec/tests/webdb.test.js
+++ spec/tests/webdb.test.js
@@ -1,18 +1,10 @@
 var webdb = require('../../index.js')
-var db = webdb('http://admin:mysecretpassword@localhost:5984')
 
-var name = 'spec'
-var collection
-
-async function createIndex(fields) {
-  await collection.data.createIndex({
-    index: { fields: fields }
-  })
-}
+var db
 
 beforeEach(async function () {
-  collection = db(name)
-  await collection.drop()
+  db = await webdb('http://admin:mysecretpassword@localhost:5984')
+  await db('spec').drop()
 })
 
 //
@@ -20,19 +12,19 @@ beforeEach(async function () {
 //
 
 test('insert single document', async function ({ t }) {
-  var doc = await collection.set({ name: 'A' })
+  var doc = await db('spec').set({ name: 'A' })
   t.ok(doc.id)
 })
 
 test('insert mutates object with id', async function ({ t }) {
   var obj = { name: 'A' }
-  await collection.set(obj)
+  await db('spec').set(obj)
   t.ok(obj.id)
 })
 
 test('bulk insert via array', async function ({ t }) {
   var docs = [{ a: 1 }, { a: 2 }]
-  var out = await collection.set(docs)
+  var out = await db('spec').set(docs)
   t.equal(out.length, 2)
   t.ok(out[0].id && out[1].id)
 })
@@ -42,21 +34,18 @@ test('bulk insert via array', async function ({ t }) {
 //
 
 test('eq matches existing field', async function ({ t }) {
-  await collection.set({ a: 1 })
-  var r = await collection.get({ a: 1 })
-  t.ok(r)
+  await db('spec').set({ a: 1 })
+  t.ok(await db('spec').get({ a: 1 }))
 })
 
 test('eq fails on missing field', async function ({ t }) {
-  await collection.set({ a: 1 })
-  var r = await collection.get({ b: 1 })
-  t.equal(r, null)
+  await db('spec').set({ a: 1 })
+  t.equal(await db('spec').get({ b: 1 }), null)
 })
 
 test('null distinct from missing', async function ({ t }) {
-  await collection.set([{ a: null }, {}])
-  var r = await collection.all({ a: null })
-  t.equal(r.length, 1)
+  await db('spec').set([{ a: null }, {}])
+  t.equal((await db('spec').all({ a: null })).length, 1)
 })
 
 //
@@ -64,13 +53,13 @@ test('null distinct from missing', async function ({ t }) {
 //
 
 test('$gt $gte $lt $lte', async function ({ t }) {
-  await createIndex(['n'])
-  await collection.set([{ n: 1 }, { n: 2 }, { n: 3 }])
+  await db('spec').index({ index: { fields: ['n'] } })
+  await db('spec').set([{ n: 1 }, { n: 2 }, { n: 3 }])
 
-  t.equal(await collection.count({ n: { $gt: 1 } }), 2)
-  t.equal(await collection.count({ n: { $gte: 2 } }), 2)
-  t.equal(await collection.count({ n: { $lt: 3 } }), 2)
-  t.equal(await collection.count({ n: { $lte: 2 } }), 2)
+  t.equal(await db('spec').count({ n: { $gt: 1 } }), 2)
+  t.equal(await db('spec').count({ n: { $gte: 2 } }), 2)
+  t.equal(await db('spec').count({ n: { $lt: 3 } }), 2)
+  t.equal(await db('spec').count({ n: { $lte: 2 } }), 2)
 })
 
 //
@@ -78,15 +67,15 @@ test('$gt $gte $lt $lte', async function ({ t }) {
 //
 
 test('$in', async function ({ t }) {
-  await createIndex(['a'])
-  await collection.set([{ a: 1 }, { a: 2 }])
-  t.equal(await collection.count({ a: { $in: [2] } }), 1)
+  await db('spec').index({ index: { fields: ['a'] } })
+  await db('spec').set([{ a: 1 }, { a: 2 }])
+  t.equal(await db('spec').count({ a: { $in: [2] } }), 1)
 })
 
 test('$nin matches existing non-equal only', async function ({ t }) {
-  await createIndex(['a'])
-  await collection.set([{ a: 1 }, { a: 2 }])
-  t.equal(await collection.count({ a: { $nin: [1] } }), 1)
+  await db('spec').index({ index: { fields: ['a'] } })
+  await db('spec').set([{ a: 1 }, { a: 2 }])
+  t.equal(await db('spec').count({ a: { $nin: [1] } }), 1)
 })
 
 //
@@ -94,15 +83,15 @@ test('$nin matches existing non-equal only', async function ({ t }) {
 //
 
 test('$regex string', async function ({ t }) {
-  await createIndex(['a'])
-  await collection.set([{ a: 'abc' }, { a: 'def' }])
-  t.equal(await collection.count({ a: { $regex: '^a' } }), 1)
+  await db('spec').index({ index: { fields: ['a'] } })
+  await db('spec').set([{ a: 'abc' }, { a: 'def' }])
+  t.equal(await db('spec').count({ a: { $regex: '^a' } }), 1)
 })
 
 test('$regex non-string fails', async function ({ t }) {
-  await createIndex(['a'])
-  await collection.set({ a: 1 })
-  t.equal(await collection.count({ a: { $regex: '1' } }), 0)
+  await db('spec').index({ index: { fields: ['a'] } })
+  await db('spec').set({ a: 1 })
+  t.equal(await db('spec').count({ a: { $regex: '1' } }), 0)
 })
 
 //
@@ -110,15 +99,15 @@ test('$regex non-string fails', async function ({ t }) {
 //
 
 test('$exists true', async function ({ t }) {
-  await createIndex(['a'])
-  await collection.set([{ a: null }, {}])
-  t.equal(await collection.count({ a: { $exists: true } }), 1)
+  await db('spec').index({ index: { fields: ['a'] } })
+  await db('spec').set([{ a: null }, {}])
+  t.equal(await db('spec').count({ a: { $exists: true } }), 1)
 })
 
 test('$exists false', async function ({ t }) {
-  await createIndex(['a'])
-  await collection.set([{ a: null }, {}])
-  t.equal(await collection.count({ a: { $exists: false } }), 1)
+  await db('spec').index({ index: { fields: ['a'] } })
+  await db('spec').set([{ a: null }, {}])
+  t.equal(await db('spec').count({ a: { $exists: false } }), 1)
 })
 
 //
@@ -126,27 +115,25 @@ test('$exists false', async function ({ t }) {
 //
 
 test('$and', async function ({ t }) {
-  await createIndex(['a', 'b'])
-  await collection.set([
+  await db('spec').index({ index: { fields: ['a', 'b'] } })
+  await db('spec').set([
     { a: 1, b: 1 },
     { a: 1, b: 2 }
   ])
-
-  t.equal(await collection.count({ $and: [{ a: 1 }, { b: 2 }] }), 1)
+  t.equal(await db('spec').count({ $and: [{ a: 1 }, { b: 2 }] }), 1)
 })
 
 test('$or', async function ({ t }) {
-  await createIndex(['a'])
-  await createIndex(['b'])
-  await collection.set([{ a: 1 }, { b: 1 }])
-
-  t.equal(await collection.count({ $or: [{ a: 1 }, { b: 1 }] }), 2)
+  await db('spec').index({ index: { fields: ['a'] } })
+  await db('spec').index({ index: { fields: ['b'] } })
+  await db('spec').set([{ a: 1 }, { b: 1 }])
+  t.equal(await db('spec').count({ $or: [{ a: 1 }, { b: 1 }] }), 2)
 })
 
 test('$not', async function ({ t }) {
-  await createIndex(['a'])
-  await collection.set([{ a: 1 }, { a: 2 }])
-  t.equal(await collection.count({ $not: { a: 1 } }), 1)
+  await db('spec').index({ index: { fields: ['a'] } })
+  await db('spec').set([{ a: 1 }, { a: 2 }])
+  t.equal(await db('spec').count({ $not: { a: 1 } }), 1)
 })
 
 //
@@ -154,17 +141,14 @@ test('$not', async function ({ t }) {
 //
 
 test('sort ascending', async function ({ t }) {
-  await createIndex(['a'])
-  await collection.set([{ a: 2 }, { a: 1 }])
-
-  var out = await collection.all({}, { sort: { a: 1 } })
-  t.equal(out[0].a, 1)
+  await db('spec').index({ index: { fields: ['a'] } })
+  await db('spec').set([{ a: 2 }, { a: 1 }])
+  t.equal((await db('spec').all({}, { sort: { a: 1 } }))[0].a, 1)
 })
 
 test('skip + limit', async function ({ t }) {
-  await collection.set([{ n: 1 }, { n: 2 }, { n: 3 }])
-  var out = await collection.all({}, { skip: 1, limit: 1 })
-  t.equal(out.length, 1)
+  await db('spec').set([{ n: 1 }, { n: 2 }, { n: 3 }])
+  t.equal((await db('spec').all({}, { skip: 1, limit: 1 })).length, 1)
 })
 
 //
@@ -172,9 +156,9 @@ test('skip + limit', async function ({ t }) {
 //
 
 test('fields include only', async function ({ t }) {
-  await collection.set({ a: 1, b: 2 })
-  var out = await collection.all({}, { fields: { a: true } })
-  t.deepEqual(Object.keys(out[0]).sort(), ['a', 'id'])
+  await db('spec').set({ a: 1, b: 2 })
+  var keys = Object.keys((await db('spec').all({}, { fields: { a: true } }))[0])
+  t.deepEqual(keys.sort(), ['a', 'id'])
 })
 
 //
@@ -182,8 +166,8 @@ test('fields include only', async function ({ t }) {
 //
 
 test('count returns only count', async function ({ t }) {
-  await collection.set([{ a: 1 }, { a: 1 }])
-  t.equal(await collection.count({ a: 1 }), 2)
+  await db('spec').set([{ a: 1 }, { a: 1 }])
+  t.equal(await db('spec').count({ a: 1 }), 2)
 })
 
 //
@@ -191,23 +175,20 @@ test('count returns only count', async function ({ t }) {
 //
 
 test('update shallow merge', async function ({ t }) {
-  await collection.set({ a: 1, b: 2 })
-  var r = await collection.set({ a: 1 }, { b: 3 })
-  t.equal(r.n, 1)
+  await db('spec').set({ a: 1, b: 2 })
+  t.equal((await db('spec').set({ a: 1 }, { b: 3 })).n, 1)
 })
 
 test('update undefined removes field', async function ({ t }) {
-  await collection.set({ a: 1, b: 2 })
-  await collection.set({ a: 1 }, { b: undefined })
-  var d = await collection.get({ a: 1 })
-  t.equal('b' in d, false)
+  await db('spec').set({ a: 1, b: 2 })
+  await db('spec').set({ a: 1 }, { b: undefined })
+  t.equal('b' in (await db('spec').get({ a: 1 })), false)
 })
 
 test('update null sets null', async function ({ t }) {
-  await collection.set({ a: 1 })
-  await collection.set({ a: 1 }, { b: null })
-  var d = await collection.get({ a: 1 })
-  t.ok(d.hasOwnProperty('b'))
+  await db('spec').set({ a: 1 })
+  await db('spec').set({ a: 1 }, { b: null })
+  t.ok((await db('spec').get({ a: 1 })).hasOwnProperty('b'))
 })
 
 //
@@ -215,13 +196,12 @@ test('update null sets null', async function ({ t }) {
 //
 
 test('delete via set(query, null)', async function ({ t }) {
-  await collection.set({ a: 1 })
-  var r = await collection.set({ a: 1 }, null)
-  t.equal(r.n, 1)
+  await db('spec').set({ a: 1 })
+  t.equal((await db('spec').set({ a: 1 }, null)).n, 1)
 })
 
 test('clear via set({}, null)', async function ({ t }) {
-  await collection.set([{ a: 1 }, { a: 2 }])
-  await collection.set({}, null)
-  t.equal(await collection.count({}), 0)
+  await db('spec').set([{ a: 1 }, { a: 2 }])
+  await db('spec').set({}, null)
+  t.equal(await db('spec').count({}), 0)
 })
